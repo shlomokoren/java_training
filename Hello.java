61237804e225c69dcefc89b4df5d6c708f884888
shlomokoren <shlomo.koren@gmail.com>
2017-03-24 14:51:07 +0300

    rename Hello.class

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello{
 	public static void main(String[] args){
 	  for(int i=1 ; i<=10;i++)	
 	  { 
-           System.out.println("Hello David  ..."+i);
+           System.out.println("New day is good day  ..."+i);
            }
 		
 	}
---
+++ sendToGit.bat
@@ -0,0 +1,7 @@
+#git init
+#git remote add origin https://github.com/shlomokoren/java_training.git
+git status
+git add .
+git status
+git commit -m "rename Hello.class"
+git push -u origin master
